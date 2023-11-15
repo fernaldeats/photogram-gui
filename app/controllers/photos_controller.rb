3cf5ee16d83ea653c42906f50ea730d052f0913d
fernaldeats <42142633+fernaldeats@users.noreply.github.com>
2023-11-15 00:21:10 +0000

    photo create and delete works

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -12,4 +12,33 @@ class PhotosController < ApplicationController
     render({ :template => "photo_templates/show" })
   end
 
+  def create
+    new_photo = Photo.new
+    new_photo.image = params.fetch("input_image")
+    new_photo.image = params.fetch("input_caption")
+    new_photo.owner_id = params.fetch("input_owner_id")
+    new_photo.save
+    
+    redirect_to("/photos/#{new_photo.id}")
+  end
+
+  def update
+    the_id = params.fetch("update_id")
+    photo = Photo.find(the_id)
+    photo.image = params.fetch("input_image")
+    photo.image = params.fetch("input_caption")
+    photo.save
+    
+    redirect_to("/photos/#{photo.id}")
+  end
+
+  def destroy
+    the_id = params.fetch("delete_id")
+    matching_photos = Photo.where({ :id => the_id })
+    the_photo = matching_photos.first
+    the_photo.destroy
+    
+    redirect_to("/photos")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   get("/photos", { :controller => "photos", :action => "index" })
   get("/photos/:path_photo_id", { :controller => "photos", :action => "show" })
+  post("/create_photo", { :controller => "photos", :action => "create" })
+  post("/update_photo/:update_id", { :controller => "photos", :action => "update" })
+  get("/delete_photo/:delete_id", { :controller => "photos", :action => "destroy" })
 end
